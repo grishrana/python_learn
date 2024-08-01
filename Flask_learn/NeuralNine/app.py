3149498e755cb1ac3212d68a54268b4a5a3031d6
grishrana <grishrana6@gmail.com>
2024-08-01 18:37:20 +0545

    Flask templates,filters,redirect,JINJA

--- Flask_learn/NeuralNine/app.py
+++ Flask_learn/NeuralNine/app.py
@@ -1,44 +1,35 @@
-from flask import Flask, request, make_response
+from flask import Flask, render_template,redirect ,url_for
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def index():
-    return "<h1>Hello World</h1>"
+    myvalue="Grish Rana"
+    myresult = 399
+    mylist = [10,20,30]
+    return render_template('index.html',mylist=mylist)
 
-@app.route('/hello', methods=['POST','GET'])
-def hello():
-    if request.method=='GET':
-        return 'You made a GET request'
-    elif request.method=="POST":
-        return "You Made a POST request"
-    else:
-        return "You will never see this message.\n"
+@app.route('/otherpage')
+def otherpage():
+    some_text="Hello World"
+    return render_template('otherpage.html',some_text=some_text)
 
-@app.route('/statusCode')
-def statusCode():
-    response = make_response("Hello world")
-    response.status_code = 200
-    response.headers['content-type']="text/plain"
-    return response
+@app.route('/redirectToOtherpage')
+def redirectToOtherpage():
+    return redirect(url_for('otherpage'))
 
-@app.route('/greet/<name>', methods=['POST','GET'])
-def greet(name):
-    return (f"Hello {name}")
 
-@app.route('/add/<int:n1>/<int:n2>')
-def add(n1,n2):
-    return f"{n1}+{n2}={n1+n2}"
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
 
-@app.route('/handle_url_params')
-def handle_params():
-    if('greeting' in request.args.keys()) & ('name' in request.args.keys()):
-        greeting = request.args['greeting']
-        name = request.args['name']
-        return f"Hello {name}"
+@app.template_filter('repeat')
+def repeat(s,times=2):
+    return s*times
 
-    else:
-        return "Some parameters are missing."
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return ''.join([c.upper() if i%2 == 0 else c.lower() for i,c in enumerate(s)])
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555,debug=True)
+if(__name__ == "__main__"):
+    app.run(host="0.0.0.0", port=5555, debug=True)
